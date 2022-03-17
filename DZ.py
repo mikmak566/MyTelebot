8c43ea9e1b7806fc756233102e90633302020f7e
shvec <a-s-w@yandex.ru>
2022-03-17 20:48:11 +0300

    Версия 004, добавил пояснения и обновил имена переменных и процедур

--- DZ.py
+++ DZ.py
@@ -13,35 +13,38 @@ def dz4(bot, chat_id):
     bot.send_message(chat_id, text="ДОДЕЛАТЬ")
 # -----------------------------------------------------------------------
 def dz5(bot, chat_id):
-    my_inputInt(bot, chat_id, "Сколько вам лет?", dz5_proc_answer)
+    my_inputInt(bot, chat_id, "Сколько вам лет?", dz5_ResponseHandler)
 
-def dz5_proc_answer(bot, chat_id, age_int):
+def dz5_ResponseHandler(bot, chat_id, age_int):
     bot.send_message(chat_id, text=f"О! тебе уже {age_int}! \nА через год будет уже {age_int+1}!!!")
 # -----------------------------------------------------------------------
 def dz6(bot, chat_id):
-    proc_answer = lambda message: bot.send_message(chat_id, f"Добро пожаловать {message.text}! У тебя красивое имя, в нём {len(message.text)} букв!")
-    my_input(bot, chat_id, "Как тебя зовут?", proc_answer)
+    dz6_ResponseHandler = lambda message: bot.send_message(chat_id, f"Добро пожаловать {message.text}! У тебя красивое имя, в нём {len(message.text)} букв!")
+    my_input(bot, chat_id, "Как тебя зовут?", dz6_ResponseHandler)
 
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
-def my_input(bot, chat_id, txt, proc_answer):
+def my_input(bot, chat_id, txt, ResponseHandler):
     message = bot.send_message(chat_id, text=txt)
-    bot.register_next_step_handler(message, proc_answer)
+    bot.register_next_step_handler(message, ResponseHandler)
 # -----------------------------------------------------------------------
-def my_inputInt(bot, chat_id, txt, proc_answer):
+def my_inputInt(bot, chat_id, txt, ResponseHandler):
 
     message = bot.send_message(chat_id, text=txt)
-    bot.register_next_step_handler(message, my_inputInt_return, botQuestion=bot, txtQuestion=txt, proc_answer=proc_answer)
-    # bot.register_next_step_handler(message, my_inputInt_return, bot, txt, proc_answer)  # то-же самое, но короче
+    bot.register_next_step_handler(message, my_inputInt_SecondPart, botQuestion=bot, txtQuestion=txt, ResponseHandler=ResponseHandler)
+    # bot.register_next_step_handler(message, my_inputInt_return, bot, txt, ResponseHandler)  # то-же самое, но короче
 
-def my_inputInt_return(message, botQuestion, txtQuestion, proc_answer):
+def my_inputInt_SecondPart(message, botQuestion, txtQuestion, ResponseHandler):
     chat_id = message.chat.id
     try:
         var_int = int(message.text)
-        proc_answer(botQuestion, chat_id, var_int)
+        # данные корректно преобразовались в int, можно вызвать обработчик ответа, и передать туда наше число
+        ResponseHandler(botQuestion, chat_id, var_int)
     except ValueError:
         botQuestion.send_message(chat_id,
                          text="Можно вводить ТОЛЬКО целое число в десятичной системе исчисления (символами от 0 до 9)!\nПопробуйте еще раз...")
-        my_inputInt(botQuestion, chat_id, txtQuestion, proc_answer)
+        my_inputInt(botQuestion, chat_id, txtQuestion, ResponseHandler)  # это не рекурсия, но очень похоже
+        # у нас пара процедур, которые вызывают друг-друга, пока пользователь не введёт корректные данные,
+        # и тогда этот цикл прервётся, и управление перейдёт "наружу", в ResponseHandler
 
 # -----------------------------------------------------------------------
