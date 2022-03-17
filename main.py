8454f99d309fd079066c17107f36a8015e0d7847
shvec <a-s-w@yandex.ru>
2022-03-18 00:09:36 +0300

    Версия 004, добавил get_ManOrNot, обновил обработчики ошибок, мелкие помарки

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Телеграм-бот v.004
 import json
+from io import BytesIO
 
 import telebot  # pyTelegramBotAPI 4.3.1
 from telebot import types
@@ -12,133 +13,170 @@ import DZ  # домашнее задание от первого урока
 bot = telebot.TeleBot('5149965447:AAF3z_oB2sM6Cch1scdWe5AV_aAV3bWmXiA')  # Создаем экземпляр бота
 game21 = None  # класс игры в 21, экземпляр создаём только при начале игры
 
+
 # -----------------------------------------------------------------------
 # Функция, обрабатывающая команды
 @bot.message_handler(commands="start")
 def command(message, res=False):
-   txt_message = f"Привет, {message.from_user.first_name}! Я тестовый бот для курса программирования на языке Python"
-   bot.send_message(message.chat.id, text=txt_message, reply_markup=Menu.getMenu("Главное меню").markup)
+    txt_message = f"Привет, {message.from_user.first_name}! Я тестовый бот для курса программирования на языке Python"
+    bot.send_message(message.chat.id, text=txt_message, reply_markup=Menu.getMenu("Главное меню").markup)
 
 
 # -----------------------------------------------------------------------
 # Получение сообщений от юзера
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-   global game21
+    global game21
 
-   chat_id = message.chat.id
-   ms_text = message.text
+    chat_id = message.chat.id
+    ms_text = message.text
 
-   result = goto_menu(chat_id, ms_text)  # попытаемся использовать текст как команду меню, и войти в него
-   if result == True:
-       return  # мы вошли в подменю, и дальнейшая обработка не требуется
+    result = goto_menu(chat_id, ms_text)  # попытаемся использовать текст как команду меню, и войти в него
+    if result == True:
+        return  # мы вошли в подменю, и дальнейшая обработка не требуется
 
-   if Menu.cur_menu != None and ms_text in Menu.cur_menu.buttons: # проверим, что команда относится к текущему меню
+    if Menu.cur_menu != None and ms_text in Menu.cur_menu.buttons:  # проверим, что команда относится к текущему меню
 
-       if ms_text == "Помощь":
-           send_help(chat_id)
+        if ms_text == "Помощь":
+            send_help(chat_id)
 
-       elif ms_text == "Прислать собаку":
-           bot.send_photo(chat_id, photo=get_dogURL(), caption="Вот тебе собачка!")
+        elif ms_text == "Прислать собаку":
+            bot.send_photo(chat_id, photo=get_dogURL(), caption="Вот тебе собачка!")
 
-       elif ms_text == "Прислать анекдот":
-           bot.send_message(chat_id, text=get_anekdot())
+        elif ms_text == "Прислать анекдот":
+            bot.send_message(chat_id, text=get_anekdot())
 
-       elif ms_text == "Карту!":
-           if game21 == None:  # если мы случайно попали в это меню, а объекта с игрой нет
-               goto_menu(chat_id, "Выход")
-               return
+        elif ms_text == "Угадай кто?":
+            get_ManOrNot(chat_id)
 
-           text_game = game21.get_cards(1)
-           bot.send_media_group(chat_id, media=getMediaCards(game21))  # получим и отправим изображения карт
-           bot.send_message(chat_id, text=text_game)
+        elif ms_text == "Карту!":
+            if game21 == None:  # если мы случайно попали в это меню, а объекта с игрой нет
+                goto_menu(chat_id, "Выход")
+                return
 
-           if game21.status != None:  # выход, если игра закончена
-               goto_menu(chat_id, "Выход")
-               return
+            text_game = game21.get_cards(1)
+            bot.send_media_group(chat_id, media=getMediaCards(game21))  # получим и отправим изображения карт
+            bot.send_message(chat_id, text=text_game)
 
-       elif ms_text == "Стоп!":
-           game21 = None
-           goto_menu(chat_id, "Выход")
-           return
+            if game21.status != None:  # выход, если игра закончена
+                goto_menu(chat_id, "Выход")
+                return
 
-       elif ms_text == "Задание-1":
-           DZ.dz1(bot, chat_id)
+        elif ms_text == "Стоп!":
+            game21 = None
+            goto_menu(chat_id, "Выход")
+            return
 
-       elif ms_text == "Задание-2":
-           DZ.dz2(bot, chat_id)
+        elif ms_text == "Задание-1":
+            DZ.dz1(bot, chat_id)
 
-       elif ms_text == "Задание-3":
-           DZ.dz3(bot, chat_id)
+        elif ms_text == "Задание-2":
+            DZ.dz2(bot, chat_id)
 
-       elif ms_text == "Задание-4":
-           DZ.dz4(bot, chat_id)
+        elif ms_text == "Задание-3":
+            DZ.dz3(bot, chat_id)
 
-       elif ms_text == "Задание-5":
-           DZ.dz5(bot, chat_id)
+        elif ms_text == "Задание-4":
+            DZ.dz4(bot, chat_id)
 
-       elif ms_text == "Задание-6":
-           DZ.dz6(bot, chat_id)
+        elif ms_text == "Задание-5":
+            DZ.dz5(bot, chat_id)
 
-   else:  # ...........................................................................................................
-       bot.send_message(chat_id, text="Мне жаль, я не понимаю вашу команду: " + ms_text)
-       goto_menu(chat_id, "Главное меню")
+        elif ms_text == "Задание-6":
+            DZ.dz6(bot, chat_id)
+
+    else:  # ...........................................................................................................
+        bot.send_message(chat_id, text="Мне жаль, я не понимаю вашу команду: " + ms_text)
+        goto_menu(chat_id, "Главное меню")
+
+# -----------------------------------------------------------------------
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "ManOrNot_GoToSite": #call.data это callback_data, которую мы указали при объявлении InLine-кнопки
+        # это способ открывать url-ссылки без вопросов от телеграмма
+        import webbrowser
+        webbrowser.open("https://vc.ru/dev/58543-thispersondoesnotexist-sayt-generator-realistichnyh-lic")
+        # После обработки каждого запроса нужно вызвать метод answer_callback_query, чтобы Telegram понял, что запрос обработан.
+        bot.answer_callback_query(call.id)
+
 # -----------------------------------------------------------------------
 def goto_menu(chat_id, name_menu):
-
     # получение нужного элемента меню
-   if name_menu == "Выход" and Menu.cur_menu != None and Menu.cur_menu.parent != None:
-       target_menu = Menu.getMenu(Menu.cur_menu.parent.name)
-   else:
-       target_menu = Menu.getMenu(name_menu)
+    if name_menu == "Выход" and Menu.cur_menu != None and Menu.cur_menu.parent != None:
+        target_menu = Menu.getMenu(Menu.cur_menu.parent.name)
+    else:
+        target_menu = Menu.getMenu(name_menu)
 
-   if target_menu != None:
-       bot.send_message(chat_id, text=target_menu.name, reply_markup=target_menu.markup)
+    if target_menu != None:
+        bot.send_message(chat_id, text=target_menu.name, reply_markup=target_menu.markup)
 
-       # Проверим, нет ли обработчика для самого меню. Если есть - выполним нужные команды
-       if target_menu.name == "Игра в 21":
-           global game21
-           game21 = BotGames.Game21()  # создаём новый экземпляр игры
-           text_game = game21.get_cards(2)  # просим 2 карты в начале игры
-           bot.send_media_group(chat_id, media=getMediaCards(game21))  # получим и отправим изображения карт
-           bot.send_message(chat_id, text=text_game)
+        # Проверим, нет ли обработчика для самого меню. Если есть - выполним нужные команды
+        if target_menu.name == "Игра в 21":
+            global game21
+            game21 = BotGames.Game21()  # создаём новый экземпляр игры
+            text_game = game21.get_cards(2)  # просим 2 карты в начале игры
+            bot.send_media_group(chat_id, media=getMediaCards(game21))  # получим и отправим изображения карт
+            bot.send_message(chat_id, text=text_game)
 
-       return True
-   else:
-       return False
+        return True
+    else:
+        return False
+
+
 # -----------------------------------------------------------------------
 def getMediaCards(game21):
-   medias = []
-   for url in game21.arr_cards_URL:
-       medias.append(types.InputMediaPhoto(url))
-   return medias
+    medias = []
+    for url in game21.arr_cards_URL:
+        medias.append(types.InputMediaPhoto(url))
+    return medias
+
 
 # -----------------------------------------------------------------------
 def send_help(chat_id):
-   global bot
-   bot.send_message(chat_id, "Автор: Швец Андрей")
-   key1 = types.InlineKeyboardMarkup()
-   btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/user59387")
-   key1.add(btn1)
-   img = open('Швец Андрей.png', 'rb')
-   bot.send_photo(chat_id, img, reply_markup=key1)
+    global bot
+    bot.send_message(chat_id, "Автор: Швец Андрей")
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/user59387")
+    markup.add(btn1)
+    img = open('Швец Андрей.png', 'rb')
+    bot.send_photo(chat_id, img, reply_markup=markup)
+
 
 # -----------------------------------------------------------------------
 def get_anekdot():
-   array_anekdots = []
-   req_anek = requests.get('http://anekdotme.ru/random')
-   soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
-   result_find = soup.select('.anekdot_text')
-   for result in result_find:
-       array_anekdots.append(result.getText().strip())
-   return array_anekdots[0]
-
+    array_anekdots = []
+    req_anek = requests.get('http://anekdotme.ru/random')
+    if req_anek.status_code == 200:
+        soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+        result_find = soup.select('.anekdot_text')
+        for result in result_find:
+            array_anekdots.append(result.getText().strip())
+    if len(array_anekdots) > 0:
+        return array_anekdots[0]
+    else:
+        return ""
 
 # -----------------------------------------------------------------------
 def get_dogURL():
-   contents = requests.get('https://random.dog/woof.json').json()
-   return contents['url']
+    url = ""
+    req = requests.get('https://random.dog/woof.json')
+    if req.status_code == 200:
+        r_json = req.json()
+        url = r_json['url']
+        # url.split("/")[-1]
+    return url
+# -----------------------------------------------------------------------
+def get_ManOrNot(chat_id):
+    global bot
 
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Проверить", callback_data = 'ManOrNot_GoToSite')
+    markup.add(btn1)
+
+    req = requests.get("https://thispersondoesnotexist.com/image", allow_redirects=True)
+    if req.status_code == 200:
+        img = BytesIO(req.content)
+        bot.send_photo(chat_id, photo=img, reply_markup=markup, caption="Этот человек реален?")
 
 # ---------------------------------------------------------------------
 bot.polling(none_stop=True, interval=0)  # Запускаем бота
