2e39c97cfa30b7e6b091cb2f18c12343b17a29ce
shvec <a-s-w@yandex.ru>
2022-03-22 12:34:55 +0300

    Версия 005, добавил get_randomFilm, возможность передавать в обработчик inline-кнопок сложные параметры через extendedParameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Телеграм-бот v.004
 import json
+from gettext import find
 from io import BytesIO
 
 import telebot  # pyTelegramBotAPI 4.3.1
@@ -45,6 +46,9 @@ def get_text_messages(message):
 
         elif ms_text == "Прислать анекдот":
             bot.send_message(chat_id, text=get_anekdot())
+
+        elif ms_text == "Прислать фильм":
+            send_film(chat_id)
 
         elif ms_text == "Угадай кто?":
             get_ManOrNot(chat_id)
@@ -92,12 +96,12 @@ def get_text_messages(message):
 # -----------------------------------------------------------------------
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "ManOrNot_GoToSite": #call.data это callback_data, которую мы указали при объявлении InLine-кнопки
-        # это способ открывать url-ссылки без вопросов от телеграмма
-        import webbrowser
-        webbrowser.open("https://vc.ru/dev/58543-thispersondoesnotexist-sayt-generator-realistichnyh-lic")
-        # После обработки каждого запроса нужно вызвать метод answer_callback_query, чтобы Telegram понял, что запрос обработан.
-        bot.answer_callback_query(call.id)
+    # если требуется передать параметр или несколько параметров в обработчик кнопки, использовать методы Menu.getExtPar() и Menu.setExtPar()
+    pass
+    # if call.data == "ManOrNot_GoToSite": #call.data это callback_data, которую мы указали при объявлении InLine-кнопки
+    #
+    #     # После обработки каждого запроса нужно вызвать метод answer_callback_query, чтобы Telegram понял, что запрос обработан.
+    #     bot.answer_callback_query(call.id)
 
 # -----------------------------------------------------------------------
 def goto_menu(chat_id, name_menu):
@@ -141,6 +145,20 @@ def send_help(chat_id):
     img = open('Швец Андрей.png', 'rb')
     bot.send_photo(chat_id, img, reply_markup=markup)
 
+# -----------------------------------------------------------------------
+def send_film(chat_id):
+    film = get_randomFilm()
+    info_str = f"<b>{film['Наименование']}</b>\n" \
+               f"Год: {film['Год']}\n" \
+               f"Страна: {film['Страна']}\n" \
+               f"Жанр: {film['Жанр']}\n" \
+               f"Продолжительность: {film['Продолжительность']}"
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Трейлер", url=film["Трейлер_url"])
+    btn2 = types.InlineKeyboardButton(text="СМОТРЕТЬ онлайн", url=film["фильм_url"])
+    markup.add(btn1, btn2)
+    bot.send_photo(chat_id, photo=film['Обложка_url'], caption=info_str, parse_mode='HTML', reply_markup=markup)
+
 
 # -----------------------------------------------------------------------
 def get_anekdot():
@@ -170,7 +188,7 @@ def get_ManOrNot(chat_id):
     global bot
 
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text="Проверить", callback_data = 'ManOrNot_GoToSite')
+    btn1 = types.InlineKeyboardButton(text="Проверить", url="https://vc.ru/dev/58543-thispersondoesnotexist-sayt-generator-realistichnyh-lic")
     markup.add(btn1)
 
     req = requests.get("https://thispersondoesnotexist.com/image", allow_redirects=True)
@@ -179,6 +197,37 @@ def get_ManOrNot(chat_id):
         bot.send_photo(chat_id, photo=img, reply_markup=markup, caption="Этот человек реален?")
 
 # ---------------------------------------------------------------------
+def get_randomFilm():
+    url = 'https://randomfilm.ru/'
+    infoFilm = {}
+    req_film = requests.get(url)
+    soup = bs4.BeautifulSoup(req_film.text, "html.parser")
+    result_find = soup.find('div', align="center", style="width: 100%")
+    infoFilm["Наименование"] = result_find.find("h2").getText()
+    names = infoFilm["Наименование"].split(" / ")
+    infoFilm["Наименование_rus"] = names[0].strip()
+    if len(names) > 1:
+        infoFilm["Наименование_eng"] = names[1].strip()
+
+    images = []
+    for img in result_find.findAll('img'):
+        images.append(url + img.get('src'))
+    infoFilm["Обложка_url"] = images[0]
+
+    details = result_find.findAll('td')
+    infoFilm["Год"] = details[0].contents[1].strip()
+    infoFilm["Страна"] = details[1].contents[1].strip()
+    infoFilm["Жанр"] = details[2].contents[1].strip()
+    infoFilm["Продолжительность"] = details[3].contents[1].strip()
+    infoFilm["Режиссёр"] = details[4].contents[1].strip()
+    infoFilm["Актёры"] = details[5].contents[1].strip()
+    infoFilm["Трейлер_url"] = url + details[6].contents[0]["href"]
+    infoFilm["фильм_url"] = url + details[7].contents[0]["href"]
+
+    return infoFilm
+# ---------------------------------------------------------------------
+
+
 bot.polling(none_stop=True, interval=0)  # Запускаем бота
 
 print()
--- menuBot.py
+++ menuBot.py
@@ -5,6 +5,8 @@ from telebot import types
 class Menu:
     hash = {}  # тут будем накапливать все созданные экземпляры класса
     cur_menu = None  # тут будет находиться текущий экземпляр класса, текущее меню
+    extendedParameters = {}  # это место хранения дополнительных параметров для передачи в inline кнопки
+    # ПЕРЕПИСАТЬ для хранения параметров привязанных к chat_id и названию кнопки
 
     def __init__(self, name, buttons=None, parent=None, action=None):
         self.parent = parent
@@ -17,6 +19,17 @@ class Menu:
         self.markup = markup
 
         self.__class__.hash[name] = self  # в классе содержится словарь, со всеми экземплярами класса, обновим его
+
+    @classmethod
+    def getExtPar(cls, id):
+        return cls.extendedParameters.pop(id, None)
+
+    @classmethod
+    def setExtPar(cls, parameter):
+        import uuid
+        id = uuid.uuid4().hex
+        cls.extendedParameters[id] = parameter
+        return id
 
     @classmethod
     def getMenu(cls, name):
@@ -34,4 +47,4 @@ m_game_rsp = Menu("Камень, ножницы, бумага", buttons=["Кам
 
 m_DZ = Menu("ДЗ", buttons=["Задание-1", "Задание-2", "Задание-3", "Задание-4", "Задание-5", "Задание-6", "Выход"], parent=m_main)
 
-m_fun = Menu("Развлечения", buttons=["Прислать собаку", "Прислать анекдот","Выход"], parent=m_main)
+m_fun = Menu("Развлечения", buttons=["Прислать собаку", "Прислать анекдот", "Прислать фильм", "Выход"], parent=m_main)
